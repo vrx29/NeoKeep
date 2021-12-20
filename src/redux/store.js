680c573294822165e7749ec939a28a55f81aa05c
Vineet Kumar <jammackp@gmail.com>
2021-12-20 21:43:34 +0530

    Delete store.js

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import authReducer from "./reducers/authReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(authReducer,composeWithDevTools())
-
-export default store
